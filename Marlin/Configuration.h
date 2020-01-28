0f6d79668d78d7b6394a75afeeadcbdbc178a4d2
Patrick Pruneau <johnwolf159@gmail.com>
2020-01-27 21:57:43 -0500

    Set default Z offset to -2 and added firmware.bin compile with lat. ver.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -932,7 +932,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -16, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -42, -16, -2 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
